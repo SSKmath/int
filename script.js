aa6e13a1165843e06bd5187eb3efeb116a1e8d1e
SSK <anehkasi24@gmail.com>
2024-11-23 22:28:04 +0300

    9

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+const move = 4;
+
 document.getElementById('convertButton1').addEventListener('click', function() {
-    const inputText = document.getElementById('textInput1').value;
-    const upperCaseText = inputText.toUpperCase();
-    document.getElementById('outputText1').innerText = upperCaseText;
+    let inputText = document.getElementById('textarea1').value;
+    let outputText = "";
+    
+    for (let i = 0; i < inputText.length; i++)
+    {
+        let code = inputText.charCodeAt(i);
+        outputText += String.fromCharCode(code + move);
+    }
+
+    document.getElementById('textarea3').innerText = outputText;
 });
 
 document.getElementById('convertButton2').addEventListener('click', function() {
-    const inputText = document.getElementById('textInput2').value;
-    const upperCaseText = inputText.toUpperCase();
-    document.getElementById('outputText2').innerText = upperCaseText;
+    const inputText = document.getElementById('textarea2').value;
+    let outputText = "";
+    
+    for (let i = 0; i < inputText.length; i++)
+    {
+        let code = inputText.charCodeAt(i);
+        outputText += String.fromCharCode(code - move);
+    }
+
+    document.getElementById('textarea4').innerText = outputText;
 });
